a6592fd43c3549a94a9a4e3b355774e8fc6f924b
Arun Kavale <arunkavale@gmail.com>
2018-04-16 20:48:45 +0530

    Added walkins

---
+++ server/models/appiontment-model.js
@@ -0,0 +1,54 @@
+var mongoose = require('mongoose');
+const validator = require('validator');
+
+
+var Appointment = mongoose.model('Appointment',{
+    name:{
+        type:String,
+        require:true,
+        minlength:1
+    },
+    phoneNumber:{
+        type:Number,
+        require:true,
+        minlength:10
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 5,
+        validate: {
+          validator: validator.isEmail,
+          message: '{VALUE} is not a valid Email address'
+        }
+      },
+    dob: {
+        type: Date,
+        required: true,
+    },
+    address: {
+        type: String,
+        required: true,
+        minlength: 2,
+        trim: true
+    },
+    notes: {
+        type: String,
+        required: true,
+        // minlength: 2,
+      
+    },
+    Interactiontype: {
+        type: String,
+        required: true,
+        minlength: 2,
+        trim: true
+    },
+    customerId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+      }
+});
+
+module.exports={Appointment};
---
+++ server/models/walkins-model.js
@@ -0,0 +1,49 @@
+var mongoose = require('mongoose');
+
+var Walkins = mongoose.model('Walkins',{
+    customerPhoneNumber:{
+        type:Number,
+        require:true,
+        minlength:10
+    },
+    customerName: {
+        type: String,
+        required: true,
+        minlength: 2,
+        trim: true
+    },
+    address: {
+        type: String,
+        required: true,
+        minlength: 2,
+        trim: true
+    },
+    notes: {
+        type: String,
+        required: true,
+        // minlength: 2,
+        trim: true
+    },
+    timestamp: {
+        type: Date,
+        required: true,
+        // minlength: 2,
+      
+    },
+    order: {
+        type: Object,
+        required: true,
+    },
+    // grandTotal: {
+    //     type: Number,
+    //     required: true,
+    //     minlength: 2,
+    //     trim: true
+    // },
+    customerId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+      }
+});
+
+module.exports={Walkins};
