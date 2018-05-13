560b2cccc0248e507c48013bd5d1c51da6558368
Arun Kavale <arunkavale@gmail.com>
2018-05-13 16:06:47 +0530

    Changed in user

--- server.js
+++ server.js
@@ -28,6 +28,18 @@ app.use(bodyParser.json());
 
 app.use(require('./routes'));
 
+app.use((req,res,next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error); 
+});
+app.use((error, req, res, next) => {
+  res.status(error.status || 500)
+  res.json({
+    error: error.message
+    
+  })
+});
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
